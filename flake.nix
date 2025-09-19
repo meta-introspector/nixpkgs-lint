00f3b2dde28d412d90d4a3b47dff039a0e62c2c3
mike <h@solfunmeme.com>
2025-09-19 13:44:26 +0000

    feat(nixpkgs-lint): CRQ-016 - Standardize flake.nix from template

--- flake.nix
+++ flake.nix
@@ -1,55 +1,29 @@
 {
-  description = "Semantic linter for Nixpkgs using tree-sitter";
+  description = "A flake for this submodule, providing a basic development shell.";
 
   inputs = {
-    naersk = {
-      url = "github:nix-community/naersk/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, ... }:
-    let
-      inherit (nixpkgs) lib;
-      nixpkgsLintLambda = pkgs':
-        let
-          pkgs = pkgs'.__splicedPackages;
-          naersk-lib = pkgs.callPackage naersk { };
-        in
-        naersk-lib.buildPackage {
-          pname = "nixpkgs-lint";
-          root = ./.;
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bash
+            git
+            shellcheck # Add shellcheck for shell script linting
+            # Add any other common tools needed for your submodules here
+          ];
+
+          shellHook = ''
+            echo "Welcome to the development shell of this submodule!"
+          '';
         };
-    in
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          packages = {
-            default = self.packages."${system}".nixpkgs-lint;
-            nixpkgs-lint = nixpkgsLintLambda pkgs;
-          };
-
-          apps.default = utils.lib.mkApp {
-            drv = self.packages."${system}".default;
-          };
-
-          devShells.default = with pkgs; mkShell {
-            nativeBuildInputs = [ cargo cargo-edit rustc rustfmt rustPackages.clippy fzy ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
-        })
-    // {
-      overlays.default = _: prev: {
-        nixpkgs-lint = nixpkgsLintLambda prev;
-      };
-    };
+      }
+    );
 }
